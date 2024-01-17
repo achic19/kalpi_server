0f1429edf8b8e6335a9c701003326280eef7f797
Achituv <achic19@gmail.com>
2024-01-17 13:42:26 -0800

    server 6.10

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from geopandas import GeoDataFrame
 from shapely.geometry import Point
 from flask import Flask, jsonify
 from urllib.parse import urlparse
-import psycopg2
+# import psycopg2
 
 
 
@@ -46,36 +46,36 @@ def geo_code_fun(row, one_point=False):
     else:
         # The request failed
         print("Request failed")
-def write_post(sorce,ballot):
-    # 1.  Connect to PostgreSQL
-    db_url = os.getenv("postgres://default:ISE3lKgq6kfe@ep-wispy-limit-89580954-"
-                       "pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb")
-    # Use the environment variable for database URL
-
-
-    parsed_url = urlparse(db_url)
-    connection = psycopg2.connect(
-        host=parsed_url.hostname,
-        port=parsed_url.port,
-        database=parsed_url.path[1:],
-        user=parsed_url.username,
-        password=parsed_url.password
-    )
-
-    # Create a cursor
-    cursor = connection.cursor()
-
-    # Define a table and column (replace 'your_table' and 'column_name' with your actual table and column names)
-    table_name = 'sp'
-    # Insert JSON data into the table
-    cursor.execute(f'INSERT INTO SP (Adr, Ballot) VALUES ({sorce},{ballot} )')
-
-    # Commit the transaction
-    connection.commit()
-
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
+# def write_post(sorce,ballot):
+#     # 1.  Connect to PostgreSQL
+#     db_url = os.getenv("postgres://default:ISE3lKgq6kfe@ep-wispy-limit-89580954-"
+#                        "pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb")
+#     # Use the environment variable for database URL
+#
+#
+#     parsed_url = urlparse(db_url)
+#     connection = psycopg2.connect(
+#         host=parsed_url.hostname,
+#         port=parsed_url.port,
+#         database=parsed_url.path[1:],
+#         user=parsed_url.username,
+#         password=parsed_url.password
+#     )
+#
+#     # Create a cursor
+#     cursor = connection.cursor()
+#
+#     # Define a table and column (replace 'your_table' and 'column_name' with your actual table and column names)
+#     table_name = 'sp'
+#     # Insert JSON data into the table
+#     cursor.execute(f'INSERT INTO SP (Adr, Ballot) VALUES ({sorce},{ballot} )')
+#
+#     # Commit the transaction
+#     connection.commit()
+#
+#     # Close the cursor and connection
+#     cursor.close()
+#     connection.close()
 
 
 @app.route('/kalpi/<address>')
@@ -95,7 +95,7 @@ def find_kalpi(address):
 
     except:
         return 'Error to find the closet calpi'
-    write_post(str(pnt_x_y),str(nearby_ballot.loc[0].geometry))
+    # write_post(str(pnt_x_y),str(nearby_ballot.loc[0].geometry))
     return json_str
 
 
