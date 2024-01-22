8b7a14937ccc3fd2a7eac817021943d6701dc2ef
Achituv <achic19@gmail.com>
2024-01-22 07:15:24 -0800

    server 8.0

--- api/index.py
+++ api/index.py
@@ -33,9 +33,9 @@ def geo_code_fun(row, one_point=False):
     #     url = f"https://maps.googleapis.com/maps/api/geocode/json?address={json_object['street']},{json_object['city']},israel"
     # except json.JSONDecodeError:
     #     url = f"https://maps.googleapis.com/maps/api/geocode/json?address={row},israel"
-    # params = {
-    #     "key": API_KEY,
-    # }
+    params = {
+        "key": API_KEY,
+    }
     url = f"https://maps.googleapis.com/maps/api/geocode/json?address={row},israel"
     response = requests.get(url, params=params)
 
