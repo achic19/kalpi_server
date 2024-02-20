5b21c22755e474afa7794525a54181191a3468c3
Achituv Cohen <50163521+achic19@users.noreply.github.com>
2024-02-20 12:39:25 +0200

    is_in_area_9

--- api/index.py
+++ api/index.py
@@ -91,7 +91,7 @@ def find_kalpi(address):
         else:
             area = address  
         is_in_data = places_dic2[(places_dic2['location']==area) | (places_dic2['name_en']==area.lower())]
-        return(places_dic2.astype(str))
+        return(places_dic2.to_string(index=False))
         if len(is_in_data)>0:
             area_eb = is_in_data.iloc[0]['area']
             kalpiyot= add_in_data()
